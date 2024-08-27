9676a5b874ca7bf61dceb6c184f5568a947cffd7
Alan Vazquez <vazquala000@warren.k12.in.us>
2024-08-27 10:10:22 -0400

    Initial Commit

---
+++ script.py
@@ -0,0 +1,5 @@
+name = input("What is your name: ")
+droids = input("How many droids do you want to meet: ")
+wookiees = input("How many wookiees do you want to meet: ")
+
+print(name, "wants to meet", droids, "droids and", wookiees, "Wookiees.")
